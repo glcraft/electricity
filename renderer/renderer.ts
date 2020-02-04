9f1eb561584398d11088065333ae44da19a58bd1
Gabin Lefranc <gabin.lefranc@gmail.com>
2020-02-04 20:27:37 +0100

    explorer item pug external

--- renderer/renderer.ts
+++ renderer/renderer.ts
@@ -3,15 +3,15 @@ import * as path from 'path'
 import * as uuid from 'uuid/v4'
 import {extractIcon} from 'extract-icon'
 import * as utils from './utils'
+import * as pug from 'pug'
 import * as bc from './breadcrumb'
 
 
-let expl = document.getElementsByClassName("explorer")
+const explorers = document.getElementsByClassName("explorer")
+let currentExplorer = explorers[0]
 let geticon=extractIcon
 
-const firstPath = `${process.cwd()}`
-
-
+const firstPath = process.cwd()
 
 const urlFolderPng=utils.getResourceURL("Folder.png")
 const urlFilePng=utils.getResourceURL("File.png")
@@ -20,12 +20,15 @@ class FileInfoPug
 {
     type: string;
     img: string;
+    name: string;
 }
 
 function gotoFolder(currentPath: string)
 {
+    const pugItem = pug.compileFile(path.join(utils.renderer_path.views, "explorers", "list", "item.pug"))
     currentPath = path.resolve(currentPath)
-    let lsFilesToPug = new Array<FileInfoPug>();
+    // let lsFilesToPug = new Array<FileInfoPug>();
+    bc.update(currentPath);
     fs.readdir(currentPath,(err, files)=>{
         if (err)
         {
@@ -38,6 +41,7 @@ function gotoFolder(currentPath: string)
             let currentFile:FileInfoPug = new FileInfoPug();
             let pathItem = `${currentPath}/${value}`
             let stats = fs.lstatSync(pathItem)
+            currentFile.name = value;
             if (err)
                 currentFile.type="unknown"
             else if (stats.isDirectory())
@@ -50,23 +54,24 @@ function gotoFolder(currentPath: string)
                 currentFile.img=urlFilePng
                 currentFile.type="file"
             }
-            lsFilesToPug.push(currentFile)
+            currentExplorer.append(utils.stringToDom(pugItem(currentFile)))
+            // lsFilesToPug.push(currentFile)
         }
+
         ///PARTITION FOLDER/FILE
         {
-            let b = expl[0].children
+            let b = currentExplorer.children
             let i2=0
             for(let i1=0;i1<b.length;i1++)
             {
                 let v=b[i1] as HTMLElement;
                 if (v.dataset.type=="dir")
                 {
-                    expl[0].insertBefore(b[i1], b[i2]);
+                    currentExplorer.insertBefore(b[i1], b[i2]);
                     i2++
                 }
             }
         }
-        bc.update(currentPath);    
     });
     
 }
