e995559dc4f4a214881188d707a9f5402a642f00
Gabin Lefranc <gabin.lefranc@gmail.com>
2020-02-05 13:41:52 +0100

    Exploration thought breadcrumb

--- renderer/breadcrumb.ts
+++ renderer/breadcrumb.ts
@@ -1,24 +1,36 @@
-import * as utils from './utils'
 import * as path from 'path'
 import * as pug from 'pug'
+import * as utils from './utils'
+import * as explorer from './explorer'
 
 const pugBCItem = pug.compileFile(path.join(utils.renderer_path.views, "breadcrumb", "item.pug"))
 
 export function update(currentPath: string)
 {
     let nodeBC = document.getElementById("breadcrumb-explorer")
-    while (nodeBC.firstChild) {
-        nodeBC.removeChild(nodeBC.firstChild);
-    }
+    utils.clearElement(nodeBC)
     const parsedPath = path.parse(currentPath)
     let BCFields: string[]=parsedPath.dir.split(/[\\/]/g);
-    BCFields.push(parsedPath.base)
+    if (BCFields[BCFields.length-1]=="")
+        BCFields.pop()
+    if (parsedPath.base!="")
+        BCFields.push(parsedPath.base)
+    let pathButton="";
     let createBCItem = field=>{
-        let t = /(\w):/.exec(field);
-        if (t)
-            field = `Disque ${t[1]}`
+        if (pathButton!=="")
+            pathButton = path.join(pathButton, field)
+        else
+            pathButton = field+path.sep
+            
+        field = utils.getFolderName(currentPath)
+        
         let htmlBCItem = pugBCItem({name: field, sepfile: utils.getResourceURL("breadcrumb_sep.png")})
-        nodeBC.append(utils.stringToDom(htmlBCItem))
+        let nodeBCItem = utils.stringToDom(htmlBCItem);
+        let pathCurrentButton = pathButton;
+        ((nodeBCItem as HTMLElement).children[0] as HTMLElement).onclick = ()=>{
+            explorer.gotoFolder(pathCurrentButton)
+        }
+        nodeBC.append(nodeBCItem)
     }
     BCFields.forEach(createBCItem)
 }
